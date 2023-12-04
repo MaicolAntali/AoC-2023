3bd9b5b3e478c9b9180169190e6d790323928594
MaicolAntali <maicol.antali.ma@gmail.com>
2023-12-04 15:27:29 +0100

    Day01 - Part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,3 +1,5 @@
+private val words = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
 fun main() {
     fun part1(input: List<String>): Int {
         return input.sumOf {
@@ -9,14 +11,29 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        return input.sumOf { line ->
+            // Matches `words` or `digits`
+            val fistNumberRegex = Regex("(${words.joinToString("|")}|[1-9])")
+            val lastNumberRegex = Regex("(${words.joinToString("|") { it.reversed() }}|[1-9])")
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 142)
+            val first = fistNumberRegex.find(line)?.value ?: "Not Found"
+            val last = lastNumberRegex.find(line.reversed())?.value ?: "Not Found"
+
+            (first.wordToInt() * 10) + last.reversed().wordToInt()
+        }
+    }
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun String.wordToInt(): Int {
+    return if (this.length == 1) {
+        this.toInt()
+    } else {
+        val indexOf = words.indexOf(this)
+        if (indexOf == -1) error("Unable to convert: $this into a number.")
+        indexOf + 1
+    }
 }
